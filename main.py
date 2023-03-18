86ac722c067e2782a67391c58f1b60039992d5da
superlover <126624921+superl0ver@users.noreply.github.com>
2023-03-18 18:48:50 +0000

    A32 done

--- main.py
+++ main.py
@@ -3,21 +3,25 @@ def main():
     # Comlete your code here
     ##################################################
     email = input('Enter your string ')
- 
-flag = True
-if not email[0].isalpha():
-    flag = False
-  lenemail = len(email)
-if lenemail <= 5 or lenemail >= 30:
-    flag = False
-if email.find('@') == -1:
-    flag = False
-else:
-    atidx = email.find('@')
-if email[atidx+ 1:].find('.') == -1:
-    flag = False
+  
+    email = input("Enter your email address: ")
+    flag = True 
 
-print (flag)
+    if not email[0].isalpha(): 
+        flag = False
+        
+    lenemail = len(email)
+    if lenemail <= 5 or lenemail >= 30:
+        flag = False 
+        
+    if email.find('@') == -1:
+        flag = False 
+    else:
+        atidx = email.find('@')
+        if email[atidx+1:].find('.') == -1:
+            flag = False
+
+    print(flag)
 
 if __name__ == '__main__':
-  main()
+    main()
